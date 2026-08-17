94432c0f644c9dfe4d89530a0df58200e58aa45f
Felipeysz <Felipesantana87146520@gmail.com>
2026-08-16 23:50:08 -0300

    fix: SaveChangesAsync retorna bool em vez de int

--- src/GRA.Domain/Repositories/IRepository.cs
+++ src/GRA.Domain/Repositories/IRepository.cs
@@ -9,5 +9,5 @@ public interface IRepository<T> where T : Entity
     Task AddAsync(T entity);
     void Update(T entity);
     void Remove(T entity);
-    Task<int> SaveChangesAsync();
+    Task<bool> SaveChangesAsync();
 }
