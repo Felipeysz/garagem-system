095249560476314fb714527b4c5294e2597941fb
Felipeysz <Felipesantana87146520@gmail.com>
2026-08-16 23:51:37 -0300

    feat: adiciona SingleAsync e SearchAsync na IRepository com Expression<Func<T,bool>>

--- src/GRA.Domain/Repositories/IRepository.cs
+++ src/GRA.Domain/Repositories/IRepository.cs
@@ -1,4 +1,5 @@
-﻿using GRA.Domain.Entities.Commom;
+﻿using System.Linq.Expressions;
+using GRA.Domain.Entities.Commom;
 
 namespace GRA.Domain.Repositories;
 
@@ -6,6 +7,8 @@ public interface IRepository<T> where T : Entity
 {
     Task<T?> GetByIdAsync(long id);
     Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> SingleAsync(Expression<Func<T, bool>> predicate);
+    Task<IEnumerable<T>> SearchAsync(Expression<Func<T, bool>> predicate);
     Task AddAsync(T entity);
     void Update(T entity);
     void Remove(T entity);
